eab604e02f745919667349dc03d05d683967bbbd
K-gultom <yehezkielgg28@gmail.com>
2024-07-12 13:37:13 +0700

    add newest

--- SC-Laravel/Sc-mon-fri-1700-1830/projek1/app/Http/Controllers/logicController.php
+++ SC-Laravel/Sc-mon-fri-1700-1830/projek1/app/Http/Controllers/logicController.php
@@ -3,21 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\AssignOp\Minus;
 
 class logicController extends Controller
 {
 
     public function index(){
 
-        $nama = "Yehezkiel Gultom";  // tipe string
-        $usia = 27; //tipe integer
-        $nilai_Double = 0.12; //tipe data float / double
-        $huruf_Pertama = 'A';
+        // $nama = "Yehezkiel Gultom";  // tipe string
+        // $usia = 27; //tipe integer
+        // $nilai_Double = 0.12; //tipe data float / double
+        // $huruf_Pertama = 'A';
 
-        echo "Nama : " . $nama . "<br>";
-        echo "Usia : " . $usia . "<br>";
-        echo "Nilai Douuble : " . $nilai_Double . "<br>";
-        echo "Huruf Pertama Nama : " . $huruf_Pertama . "<br>";
+        // echo "Nama : " . $nama . "<br>";
+        // echo "Usia : " . $usia . "<br>";
+        // echo "Nilai Douuble : " . $nilai_Double . "<br>";
+        // echo "Huruf Pertama Nama : " . $huruf_Pertama . "<br>";
+
+        //============================================================================
+
+        // $saklar = 0;
+
+        // if ($saklar == 1) {
+        //     $pesan = 'Lampu Hidup';
+        // } else {
+        //     $pesan = 'Lampu Mati';
+        // }
+        
+        // echo $pesan;
+
+        //============================================================================
+        
+
+        // $usia = 1;
+
+        // if ($usia <= 5 && $usia >= 0) {
+        //     $pesan = "Usia Balita";
+        // } else {
+        //     $pesan = "Usia Sudah Lewat Balita";
+        // }
+
+        // echo $pesan;
+        
+        //============================================================================
+        
+        
+        
+
+
+
+
+
 
     }
 
