52a560f7481649ce8b198dc54c80cf6ba4db9785
K-gultom <yehezkielgg28@gmail.com>
2024-07-31 23:09:51 +0700

    commit

--- testing-pct/SimpleEcommerce/routes/web.php
+++ testing-pct/SimpleEcommerce/routes/web.php
@@ -1,18 +1,61 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\EtalaseController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Middleware\NoLogin;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware([NoLogin::class])-> group(function(){
+
+    Route::controller(AuthController::class)->group(function(){
+        Route::get('/login', 'index');
+        Route::post('/login', 'store')->name('login');
+    });
+
+    Route::controller(RegisterController::class)->group(function(){
+        Route::get('/register', 'index');
+        Route::post('/register', 'store');
+    });
+    
 });
+
+
+Route::get('/login', [authController::class, 'index']);
+Route::post('/login', [authController::class, 'store'])->name('login');
+
+    Route::controller(RegisterController::class)->group(function(){
+        Route::get('/register', 'index');
+        Route::post('/register', 'store');
+    });
+
+
+Route::get('/', [HomeController::class, 'index']);
+
+
+Route::get('/etalase', [EtalaseController::class, 'index']);
+
+
+    Route::controller(KategoriController::class)->group(function(){
+        Route::get('/kategori', 'index');   //menampilkan halaman kategori
+        Route::get('/kategori/{id}', 'destroy');    //route untuk menghapus data kategori
+        Route::get('/kategori/add', 'create');  //untuk menampilkan form simpan data kategori
+        Route::post('/kategori/add', 'store');  // untuk proses simpan data kategori
+        Route::get('/kategori/edit/{id}', 'edit');  //untuk menampilkan form edit data kategori
+        Route::post('/kategori/edit/{id}', 'update');   //untuk proses update/edit data kategori
+    });
+
+
+    Route::controller(BarangController::class)->group(function(){
+        Route::get('/barang', 'index');
+        Route::get('/barang/{id}', 'destroy');
+        Route::get('/barang/add', 'create');
+        Route::post('/barang/add', 'store');
+        Route::get('/barang/edit/{id}', 'edit');
+        Route::post('/barang/edit/{id}', 'update');
+    });
