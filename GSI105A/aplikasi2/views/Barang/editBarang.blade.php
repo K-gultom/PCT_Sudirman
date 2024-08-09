da31316fbc7e0ea6bc0f4439aec32fdda871edba
K-gultom <yehezkielgg28@gmail.com>
2024-08-09 10:35:54 +0700

    commit

---
+++ GSI105A/aplikasi2/views/Barang/editBarang.blade.php
@@ -0,0 +1,86 @@
+@extends('main')
+@section('title')
+    Edit Product
+@endsection
+@section('content')
+    <div class="container-fluid pt-3 mb-3">
+        <h4>Edit Product</h4>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{url('/')}}">Beranda</a></li>
+              <li class="breadcrumb-item"><a href="{{url('/barang')}}">Barang</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Edit</li>
+            </ol>
+        </nav>
+        <div class="col-md-4 offset-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Edit</strong> Product
+                </div>
+                <div class="card-body">
+                    
+
+                        @if(Session::has('msg'))
+                            <div class="card alert alert-success">
+                                {{Session::get('msg')}}
+                            </div>
+                        @endif
+
+                        <form action="" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="kategori">Choose Category</label>
+                                <select name="kategori" id="kategori" 
+                                    class="form-control @error('kategori') is-invalid @enderror">
+                                        <option value="">Choose</option>
+                                    {{-- @foreach ($kategori as $item)
+                                        <option value="{{$item->id}}" @if($item->id == $data->category_id) selected @endif>{{$item->name}}</option>
+                                    @endforeach --}}
+                                </select>
+                                @error('kategori')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="name">Name</label>
+                                <input value="{{old('name')}}" type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" placeholder="Type product name ...">
+                                @error('name')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="price">Price</label>
+                                <input value="{{old('price')}}" type="number" name="price" id="price" class="form-control @error('price') is-invalid @enderror" placeholder="Type product price ...">
+                                @error('price')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="photo">Photo</label>
+                                <div class="mt-2 mb-3 w-50 align-items-center">
+                                    <img src="{{url('assets/produkImages')}}" alt="" class="img-fluid">
+                                </div>
+                                <input type="file" name="photo" id="photo" class="form-control @error('photo') is-invalid @enderror" placeholder="Choose product photo ...">
+                                @error('photo')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="bi bi-box-arrow-in-right"></i>
+                                Update
+                            </button>
+                            <a href="{{ url('/barang') }}" class="btn btn-warning">Cancel</a>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
